84f1161e692267b394ff82fd4dbe4006fb00bee8
LeoninCS <xianchaoqian@gmail.com>
2025-11-16 07:22:11 +0800

    修复前端bug

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,8 @@
 import axios from 'axios'
 
 // 统一变量名：VITE_API_BASE_URL
-const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080'
+const baseURL = import.meta.env.VITE_API_BASE || '/'
+
 
 export const api = axios.create({
   baseURL,
